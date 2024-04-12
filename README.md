308050904dd2be708d93a6e215c81a3d64c42179
sheetal <sheetalkumarilife@gmail.com>
2024-04-12 15:23:50 +0530

    manipulated div using dom

---
+++ README.md
@@ -0,0 +1,21 @@
+### DOM Project 1
+---
+[Hosting Link]( )
+
+This Project is all about manupulating <b>style</b> property of the conatiner div using javascript DOM.
+
+Following Changes where made on div ----
+
+💫 Background Color
+
+💫 Margin
+
+💫 Padding
+
+💫 Font size
+
+💫 Font Weight
+
+💫 Height
+
+💫 Width
---
+++ script.js
@@ -0,0 +1,11 @@
+const conatiner = document.getElementById("container");
+
+conatiner.style.backgroundColor ="yellow";
+conatiner.style.margin="20px";
+conatiner.style.padding="10px";
+conatiner.style.fontSize="18px";
+conatiner.style.fontWeight="bold";
+conatiner.style.height="200px";
+conatiner.style.width="300px";
+
+
